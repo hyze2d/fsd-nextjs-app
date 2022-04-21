a0bc50ae17e1b33954b26c627e45050f3665881d
ein1x3x <ein1x3x.dev@gmail.com>
2022-04-21 17:52:18 +0200

    Fix empty files

--- src/shared/form/example/login.tsx
+++ src/shared/form/example/login.tsx
@@ -1,86 +1,88 @@
-import { array, string } from 'yup';
+// import { array, string } from 'yup';
 
-const email = {
-  value: '',
-  schema: string().required().email(),
+// const email = {
+//   value: '',
+//   schema: string().required().email(),
 
-  mapTo: {
-    completed: value => value > 15
-  },
+//   mapTo: {
+//     completed: value => value > 15
+//   },
 
-  rules: [
-    {
-      validateOn: ['blur'],
+//   rules: [
+//     {
+//       validateOn: ['blur'],
 
-      name: 'availability',
+//       name: 'availability',
 
-      validate: async (value, { values, someshit }) => {
-        await checkIsFreeEmail(value);
+//       validate: async (value, { values, someshit }) => {
+//         await checkIsFreeEmail(value);
 
-        return 'Email is not free smh';
-      }
-    }
-  ]
-};
+//         return 'Email is not free smh';
+//       }
+//     }
+//   ]
+// };
 
-const password = {
-  value: '',
-  schema: string().required().min(6)
-};
+// const password = {
+//   value: '',
+//   schema: string().required().min(6)
+// };
 
-const permissions = {
-  value: [],
-  schema: array().min(1).required()
-};
+// const permissions = {
+//   value: [],
+//   schema: array().min(1).required()
+// };
 
-const $registrationForm = createForm({
-  schema: {
-    password,
-    permissions,
-    email: {
-      ...email,
-      schema: email.schema.label('Account email')
-    }
-  },
+// const $registrationForm = createForm({
+//   schema: {
+//     password,
+//     permissions,
+//     email: {
+//       ...email,
+//       schema: email.schema.label('Account email')
+//     }
+//   },
 
-  initialValues: {},
+//   initialValues: {},
 
-  onSubmit: () => {
-    // some default shit
-  }
-});
+//   onSubmit: () => {
+//     // some default shit
+//   }
+// });
 
-$registrationForm.submitted;
-$registrationForm.validating;
+// $registrationForm.submitted;
+// $registrationForm.validating;
 
-const Register = ({}) => {
-  const isNotUnique = false; // external value
-  const emailCheckInProgress = false; // external value
+// const Register = ({}) => {
+//   const isNotUnique = false; // external value
+//   const emailCheckInProgress = false; // external value
 
-  const { email, password, permissions } = useForm($registrationForm, {
-    isValidating: emailCheckInProgress,
-    mapErrors: (values, errors) => ({
-      ...errors,
-      email: isNotUnique ? 'This is email is already taken' : errors.email
-    })
-  });
+//   const { email, password, permissions } = useForm($registrationForm, {
+//     isValidating: emailCheckInProgress,
+//     mapErrors: (values, errors) => ({
+//       ...errors,
+//       email: isNotUnique ? 'This is email is already taken' : errors.email
+//     })
+//   });
 
-  return (
-    <Form>
-      <Field.Input
-        is={email}
-        onBlur={() => {
-          checkEmail();
-        }}
-      />
+//   return (
+//     <Form>
+//       <Field.Input
+//         is={email}
+//         onBlur={() => {
+//           checkEmail();
+//         }}
+//       />
 
-      <Field.Input is={password} />
+//       <Field.Input is={password} />
 
-      <Field.Input is={permissions} />
+//       <Field.Input is={permissions} />
 
-      <Button type='submit'>Submit</Button>
-    </Form>
-  );
-};
+//       <Button type='submit'>Submit</Button>
+//     </Form>
+//   );
+// };
 
-export { Login };
+// export { Login };
+
+export {};
