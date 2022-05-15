c843a26c5164f8571543cb695cd5c2f488899d79
ein1x3x <ein1x3x.dev@gmail.com>
2022-05-15 13:52:03 +0200

    feat: commitlint call

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -8,19 +8,5 @@ module.exports = {
   ],
   rules: {
     // 'jira-task-id-max-length': [2, 'always', 12],
-    'type-enum': [
-      'build',
-      'chore',
-      'ci',
-      'docs',
-      'feat',
-      'fix',
-      'perf',
-      'refactor',
-      'revert',
-      'style',
-      'test',
-      'new'
-    ]
   }
 };
