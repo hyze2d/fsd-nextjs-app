aa54ffb38594b478541b7232a1e72ee287a5556f
arxv3x <arxv3x.dev@gmail.com>
2022-06-15 21:25:51 +0200

    feat: home example

--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -1,43 +1,47 @@
-import useTranslation from 'next-translate/useTranslation';
+import { useStore } from 'effector-react';
 import { $$album } from '@entities/album';
-import { createView } from '@shared/lib/view';
-import styles from './home.module.scss';
 
-const Home = createView()
-  .props({
-    albums: $$album.featuredAlbums
-  })
+// const Home = createView()
+//   .props({
+//     albums: $$album.featuredAlbums
+//   })
 
-  .view(({ albums }) => {
-    const { t } = useTranslation();
+//   .view(({ albums }) => {
+//     const { t } = useTranslation();
 
-    return (
-      <div>
-        <h1 className={styles.title}>
-          {t('siteTitle')} / {t('home.title')}
-        </h1>
+//     return (
+//       <div>
+//         <h1 className={styles.title}>
+//           {t('siteTitle')} / {t('home.title')}
+//         </h1>
 
-        <ul className={styles.albums}>
-          {albums.map(album => (
-            <li key={album.id}>
-              <article className={styles.album}>
-                <h2 className={styles.albumTitle}>{album.title}</h2>
+//         <ul className={styles.albums}>
+//           {albums.map(album => (
+//             <li key={album.id}>
+//               <article className={styles.album}>
+//                 <h2 className={styles.albumTitle}>{album.title}</h2>
 
-                <img
-                  className={styles.albumThumbnail}
-                  src={album.thumbnail}
-                  alt='album thumbnail'
-                  width={200}
-                  height={200}
-                />
+//                 <img
+//                   className={styles.albumThumbnail}
+//                   src={album.thumbnail}
+//                   alt='album thumbnail'
+//                   width={200}
+//                   height={200}
+//                 />
 
-                <p className={styles.albumDescription}>{album.description}</p>
-              </article>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  });
+//                 <p className={styles.albumDescription}>{album.description}</p>
+//               </article>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     );
+//   });
+
+const Home = () => {
+  const albums = useStore($$album.featuredAlbums);
+
+  return <div>{albums.length}</div>;
+};
 
 export { Home };
