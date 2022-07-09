0748e022778c98eeb44bfdafc871e7ed642dcca7
ein1x3x <ein1x3x.dev@gmail.com>
2022-07-09 20:07:38 +0200

    feat: api generation and project conf

--- scripts/gen/api.js
+++ scripts/gen/api.js
@@ -1,33 +1,50 @@
+const fs = require('fs');
+
 const path = require('path');
+
+const config = require('../../project.config.json');
+
+const prettier = require('prettier');
 
 const { generateApi } = require('swagger-typescript-api');
 
-const fileName = 'api.gen.ts';
-
-const outputDir = path.resolve(process.cwd(), './src/shared/api');
-
-const urlToSwaggerSchema = 'https://api.predic8.de/shop/swagger';
+const outputDir = path.resolve(process.cwd(), `.${config.api.outputDir}`);
 
 const pathToTemplate = path.resolve(
   process.cwd(),
+
   'node_modules',
+
   'effector-http-api/codegen-template'
 );
 
-const generate = () => {
-  generateApi({
-    name: fileName,
+const generate = async () => {
+  await Promise.all(
+    config.api.sdks.map(source =>
+      generateApi({
+        name: source.fileName,
 
-    output: outputDir,
+        output: outputDir,
 
-    url: urlToSwaggerSchema,
+        url: source.swaggerUrl,
 
-    httpClientType: 'axios',
+        httpClientType: 'axios',
 
-    generateClient: true,
+        generateClient: true,
 
-    templates: pathToTemplate
-  });
+        templates: pathToTemplate
+      })
+    )
+  );
+
+  fs.writeFileSync(
+    outputDir + '/index.ts',
+    `
+      ${config.api.sdks.map(
+        item => `export * from './${item.fileName.replace('.ts', '')}'`
+      )}
+    `
+  );
 };
 
 module.exports = { generate };
---
+++ src/shared/api/sdk/index.ts
@@ -0,0 +1,3 @@
+
+      export * from './fruit-shop'
+    
