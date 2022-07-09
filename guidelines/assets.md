62cdb6fd98e22b3c13244f31a97cee0ae00f0bf2
ein1x3x <ein1x3x.dev@gmail.com>
2022-07-09 17:48:04 +0200

    fix: npm scripts

---
+++ guidelines/assets.md
@@ -0,0 +1,1 @@
+## Icons And SVG
--- src/shared/ui/icons/docs/index.stories.tsx
+++ src/shared/ui/icons/docs/index.stories.tsx
@@ -1,102 +1,128 @@
-import {
-  IconGallery as _IconGallery,
-  IconItem as _IconItem
-} from '@storybook/addon-docs';
-import type { IconItemProps } from '@storybook/components';
 import type { Meta, Story } from '@storybook/react';
-import type { FC, PropsWithChildren } from 'react';
+import type { IconItemProps } from '@storybook/components';
 
-import { CodepenIcon } from '../codepen';
 import { Icon } from '../index';
 
+import { CodepenIcon } from '../codepen';
 import { TelegramIcon } from '../telegram';
 import { TwitterIcon } from '../twitter';
 import { VkIcon } from '../vk';
 import { WhatsappIcon } from '../whatsapp';
 
+
+import type { FC, PropsWithChildren } from 'react';
+import { IconGallery as _IconGallery, IconItem as _IconItem } from '@storybook/addon-docs';
+
 const IconGallery = _IconGallery as FC<PropsWithChildren<{}>>;
 const IconItem = _IconItem as FC<PropsWithChildren<IconItemProps>>;
 
+
 export const Playground: Story = () => (
   <IconGallery>
-    <IconItem name='Icon.ChevronDown'>
-      <Icon.ChevronDown />
-    </IconItem>
+     
+        <IconItem name='Icon.ChevronDown'>
+  <Icon.ChevronDown/>
+</IconItem>
 
-    <IconItem name='Icon.ChevronLeft'>
-      <Icon.ChevronLeft />
-    </IconItem>
+     
+        <IconItem name='Icon.ChevronLeft'>
+  <Icon.ChevronLeft/>
+</IconItem>
 
-    <IconItem name='Icon.ChevronRight'>
-      <Icon.ChevronRight />
-    </IconItem>
+     
+        <IconItem name='Icon.ChevronRight'>
+  <Icon.ChevronRight/>
+</IconItem>
 
-    <IconItem name='Icon.ChevronUp'>
-      <Icon.ChevronUp />
-    </IconItem>
+     
+        <IconItem name='Icon.ChevronUp'>
+  <Icon.ChevronUp/>
+</IconItem>
 
-    <IconItem name='CodepenIcon'>
-      <CodepenIcon />
-    </IconItem>
+     
+     
+        <IconItem name='CodepenIcon'>
+  <CodepenIcon />
+</IconItem>
 
-    <IconItem name='TelegramIcon'>
-      <TelegramIcon />
-    </IconItem>
+     
+        <IconItem name='TelegramIcon'>
+  <TelegramIcon />
+</IconItem>
 
-    <IconItem name='TwitterIcon'>
-      <TwitterIcon />
-    </IconItem>
+     
+        <IconItem name='TwitterIcon'>
+  <TwitterIcon />
+</IconItem>
 
-    <IconItem name='VkIcon'>
-      <VkIcon />
-    </IconItem>
+     
+        <IconItem name='VkIcon'>
+  <VkIcon />
+</IconItem>
 
-    <IconItem name='WhatsappIcon'>
-      <WhatsappIcon />
-    </IconItem>
-  </IconGallery>
+     
+        <IconItem name='WhatsappIcon'>
+  <WhatsappIcon />
+</IconItem>
+
+       </IconGallery>
 );
+
+
 export const Sprite: Story = () => (
   <IconGallery>
-    <IconItem name='Icon.ChevronDown'>
-      <Icon.ChevronDown />
-    </IconItem>
+     
+        <IconItem name='Icon.ChevronDown'>
+  <Icon.ChevronDown/>
+</IconItem>
 
-    <IconItem name='Icon.ChevronLeft'>
-      <Icon.ChevronLeft />
-    </IconItem>
+     
+        <IconItem name='Icon.ChevronLeft'>
+  <Icon.ChevronLeft/>
+</IconItem>
 
-    <IconItem name='Icon.ChevronRight'>
-      <Icon.ChevronRight />
-    </IconItem>
+     
+        <IconItem name='Icon.ChevronRight'>
+  <Icon.ChevronRight/>
+</IconItem>
 
-    <IconItem name='Icon.ChevronUp'>
-      <Icon.ChevronUp />
-    </IconItem>
-  </IconGallery>
+     
+        <IconItem name='Icon.ChevronUp'>
+  <Icon.ChevronUp/>
+</IconItem>
+
+       </IconGallery>
 );
+
+
 export const Standalone: Story = () => (
   <IconGallery>
-    <IconItem name='CodepenIcon'>
-      <CodepenIcon />
-    </IconItem>
+      
+             <IconItem name='CodepenIcon'>
+  <CodepenIcon />
+</IconItem>
 
-    <IconItem name='TelegramIcon'>
-      <TelegramIcon />
-    </IconItem>
+      
+             <IconItem name='TelegramIcon'>
+  <TelegramIcon />
+</IconItem>
 
-    <IconItem name='TwitterIcon'>
-      <TwitterIcon />
-    </IconItem>
+      
+             <IconItem name='TwitterIcon'>
+  <TwitterIcon />
+</IconItem>
 
-    <IconItem name='VkIcon'>
-      <VkIcon />
-    </IconItem>
+      
+             <IconItem name='VkIcon'>
+  <VkIcon />
+</IconItem>
 
-    <IconItem name='WhatsappIcon'>
-      <WhatsappIcon />
-    </IconItem>
-  </IconGallery>
+      
+             <IconItem name='WhatsappIcon'>
+  <WhatsappIcon />
+</IconItem>
+
+        </IconGallery>
 );
 
 const story: Meta = {
