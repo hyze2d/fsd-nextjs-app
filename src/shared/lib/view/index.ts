f511e6a4d383b723ad59ab32b39fd50022fdefc3
ein1x3x <ein1x3x.dev@gmail.com>
2022-07-18 02:22:20 +0200

    feat: updated effector-view

--- src/shared/lib/view/index.ts
+++ src/shared/lib/view/index.ts
@@ -1,7 +1,11 @@
 import { is } from 'effector';
-import { useUnit, useStoreMap } from 'effector-react';
+import { useStoreMap, useUnit } from 'effector-react';
 import { createLib } from 'effector-view';
 
-const { createView, connect } = createLib({ useUnit, useStoreMap, is });
+const { createView, connect } = createLib({
+  useUnit,
+  useStoreMap,
+  is
+});
 
 export { createView, connect };
